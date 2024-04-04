abe56fa86ea8b6486d4676a78d85123996a32b01
ThomasGG1203 <Thomasgg1203@gmail.com>
2024-04-04 10:24:04 -0500

    Uso de los guards, introduccion

--- src/guards/browser-agent.guard.ts
+++ src/guards/browser-agent.guard.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { 
+  CanActivate, 
+  ExecutionContext, 
+  HttpException, 
+  HttpStatus, 
+  Injectable 
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,6 +12,18 @@ export class BrowserAgentGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true; //Esto viene por defecto en TRUE
+    const req = context.getArgByIndex(0);
+    const userAgent = req.headers['user-agent'];
+    const isAllowed = (userAgent === 'google/chrome')
+
+    if(!isAllowed) throw new HttpException('BROWSER_AGENT_INVALID', HttpStatus.BAD_REQUEST);
+
+    return isAllowed;
+
+    // if(userAgent !== 'google/chrome'){
+    //   return false;
+    // }
+    // console.log('_____', userAgent);//_____ Thunder Client (https://www.thunderclient.com)
+    // return true; //Esto viene por defecto en TRUE
   }
 }
